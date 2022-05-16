89e23c30f9cd22f4bf8d76eefb8eaace11f8e1e2
Fejren <68419282+Fejren@users.noreply.github.com>
2022-05-16 23:56:38 +0200

    Mock react components

---
+++ frontend/src/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchInput from "../components/Search/SearchInput";
+
+const Home = () => {
+    return (
+      <div className="home__body">
+        <h1>halo</h1>
+        <SearchInput />
+      </div>
+    );
+};
+
+export default Home;

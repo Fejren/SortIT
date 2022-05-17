576cf6d957eb145dd8d940c927f217889d6bad32
Dawid <dawid.zareba03@gmail.com>
2022-05-17 18:35:13 +0200

    Added tailwind css

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import Logo from '../../assets/images/logo.svg'
+
 const Navbar = () => {
   return (
-    <header>
-      Home
-    </header>
+  <nav class="flex items-center justify-between flex-wrap bg-gray-700 p-6">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <img src={Logo}>
+      </img>
+      <span class="font-semibold text-xl tracking-tight">SortIT</span>
+    </div>
+    <div class="block lg:hidden">
+      <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+      </button>
+    </div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow">
+      </div>
+    </div>
+  </nav>
   );
 };
 

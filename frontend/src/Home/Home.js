52dfb5f6b31308d7d97293e410f2f434c0b2bca8
Fejren <68419282+Fejren@users.noreply.github.com>
2022-05-18 20:10:51 +0200

    Added rules of segregation section and stylized input

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import RulesOfSegregation from '../components/RulesOfSegregation/RulesOfSegregation';
 import SearchInput from "../components/Search/SearchInput";
 
 const Home = () => {
-    return (
-      <div className="home__body">
-        <h1>halo</h1>
-        <SearchInput />
-      </div>
-    );
+
+  return (
+    <>
+    <div className='grid place-items-start ml-9 mt-7'>
+      <p className='text-sm font-normal text-gray-700 mb-1'>WYSZUKIWARKA ODPADÓW</p>
+      <p className='text-5xl font-medium'>Co gdzie wyrzucić?</p>
+      <p className='text-xs font-light mt-4 text-gray-700'>
+        Jeśli nie wiesz, gdzie wyrzucić dany odpad, skorzystaj z mojej wyszukiwarki. <br />
+        Wpisz nazwę przedmiotu i sprawdź, gdzie możesz go wyrzucić.
+      </p>
+    </div>
+    {/* Middle container */}
+      {/* Form */}
+    <div className='grid place-items-center rounded m-10 align-middle'>
+      <SearchInput />
+    </div>
+    {/* End of form */}
+    <RulesOfSegregation />
+    {/* End of middle container */}
+    </>
+  );
 };
 
 export default Home;
--- frontend/src/components/Search/SearchInput.js
+++ frontend/src/components/Search/SearchInput.js
@@ -16,11 +16,22 @@ const SearchInput = () => {
   }, []);
 
   return (
-    <div>
-      {trashes.map(trash => (
-        <div key={trash.id}>{trash.name}</div>
-      ))}
+    // <div>
+    //   {trashes.map(trash => (
+    //     <div key={trash.id}>{trash.name}</div>
+    //   ))}
+    // </div>
+    <>
+    <div style={{backgroundColor: "#379E6B"}} class="flex flex-wrap rounded -mx-3 mb-6 text-white w-2/4">
+      <div class="w-full px-3">
+        <label class="block uppercase tracking-wide text-xs text-gray-700 font-bold mt-3 mb-3" for="search">
+          Nazwa odpadu
+        </label>
+        <input class="appearance-none block w-full text-gray-700 bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="search" type="text" placeholder="np. butelka" />
+        <p class="text-xs italic text-gray-700 mt-3 mb-3">Jeżeli nie znajdziesz czego potrzebujesz, proszę o kontakt</p>
+      </div>
     </div>
+    </>
   );
 };
 

9d4800d7d0870ba7b0d73924a908b3155fc34979
Fejren <68419282+Fejren@users.noreply.github.com>
2022-05-19 11:46:13 +0200

    Working autocomplete

--- frontend/src/components/Search/SearchInput.js
+++ frontend/src/components/Search/SearchInput.js
@@ -1,9 +1,14 @@
 import React, {useEffect, useState} from 'react';
+import "react-widgets/styles.css";
 import axios from 'axios';
+import Combobox from "react-widgets/Combobox";
 
 const SearchInput = () => {
   const [trashes, setTrashes] = useState([])
-    useEffect(() => {
+  const [selectedTrash, setSelectedTrash] = useState(trashes[0])
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
     const fetchData = async () => {
       const trashes = await axios(
         'http://127.0.0.1:8000/api/trash/trashes/',
@@ -16,19 +21,23 @@ const SearchInput = () => {
   }, []);
 
   return (
-    // <div>
-    //   {trashes.map(trash => (
-    //     <div key={trash.id}>{trash.name}</div>
-    //   ))}
-    // </div>
     <>
-    <div style={{backgroundColor: "#379E6B"}} class="flex flex-wrap rounded -mx-3 mb-6 text-white w-2/4">
-      <div class="w-full px-3">
-        <label class="block uppercase tracking-wide text-xs text-gray-700 font-bold mt-3 mb-3" for="search">
+    <div style={{backgroundColor: "#379E6B"}} className="flex flex-wrap rounded -mx-3 mb-6 text-white w-2/4">
+      <div className="w-full px-3">
+        <label className="block uppercase tracking-wide text-xs text-gray-700 font-bold mt-3 mb-3" htmlFor="rw_4_input">
           Nazwa odpadu
         </label>
-        <input class="appearance-none block w-full text-gray-700 bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="search" type="text" placeholder="np. butelka" />
-        <p class="text-xs italic text-gray-700 mt-3 mb-3">Jeżeli nie znajdziesz czego potrzebujesz, proszę o kontakt</p>
+        <Combobox
+          data={trashes}
+          key={'id'}
+          placeholder="np. butelka"
+          textField={'name'}
+          messages={{
+            emptyList: "Podana nazwa nie istnieje w bazie danych"
+          }}
+        />
+        <p className="text-xs italic text-gray-700 mt-3 mb-3">Jeżeli nie znajdziesz czego potrzebujesz, proszę o
+          kontakt</p>
       </div>
     </div>
     </>

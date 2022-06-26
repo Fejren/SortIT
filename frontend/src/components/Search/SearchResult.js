8b161ece049190b1cd78349abf19fa5a1e7c2820
Dawid Zareba <dawid.zareba03@gmail.com>
2022-06-26 16:03:31 +0200

    Added result panel

--- frontend/src/components/Search/SearchResult.js
+++ frontend/src/components/Search/SearchResult.js
@@ -8,10 +8,10 @@ const SearchResult = query => {
         <div style={{backgroundColor: "#379E6B"}} className="p-10 gap-5 rounded overflow-hidden shadow-lg">
             <div className="px-6 py-4">
               <div className='text-center'>
-                 Znaleziono wyniki dla twojego wyszukiwania
-              </div>
-              <div className='grid grid-cols-1 gap-5 '>
-                {result.name} należy wyrzucić do pojemnika {result.trash_container.toUpperCase()}, ponieważ w jego składzie występuje/ą {result.material}
+                <p className='text-5xl font-medium'>Znaleziono wyniki dla twojego wyszukiwania!</p>
+                <p className='text-2xl font-medium mt-4 text-gray-700'>
+                  {result.name} należy wyrzucić do pojemnika {result.trash_container.toUpperCase()}
+                </p>
               </div>
             </div>
         </div>
